8ae487b4ecfcc1d4546882bac9890a3315b67f60
wichan7 <wichan7@naver.com>
2023-08-14 15:29:59 +0900

    Feature: Spring Security Config 추가

---
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -0,0 +1,33 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+	
+	@Bean
+	MvcRequestMatcher.Builder mvc(HandlerMappingIntrospector introspector) {
+		return new MvcRequestMatcher.Builder(introspector);
+	}
+
+	@Bean
+	SecurityFilterChain appSecurity(HttpSecurity http, MvcRequestMatcher.Builder mvc) throws Exception {
+	    http
+	        .authorizeHttpRequests((authorize) -> authorize
+	            .requestMatchers(mvc.pattern("/")).permitAll()
+	            .requestMatchers(mvc.pattern("/assets/**")).permitAll()
+	            .requestMatchers(mvc.pattern("/board/**")).permitAll()
+	            .anyRequest().authenticated()
+	        );
+
+	    return http.build();
+	}
+	
+}
